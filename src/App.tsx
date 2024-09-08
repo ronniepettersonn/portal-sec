f976086786b8a390f3fe48a6c1f299b017ede87d
Ronnie Pettersonn <ronniepettersonn@Ronnies-Mac-Studio.local>
2024-09-08 01:49:47 -0300

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ export function App() {
         <img src={Blur2} alt="" className='absolute left-auto top-0 right-0 bottom-auto' />
       </div>
       <div className=" py-4 flex justify-center w-full px-10">
-        <div className="flex justify-between items-center max-w-[1150px] w-full">
+        <div className="flex justify-between max-md:justify-center items-center max-w-[1150px] w-full">
           <div>
             <img src={Logo} alt="Logo" className='h-16' />
           </div>
-          <div className='text-zinc-400 gap-6 flex'>
+          <div className='text-zinc-400 gap-6 flex max-md:hidden'>
             <span>Palavra</span> <span>/</span>  <span>Edificação</span>   <span>/</span>   <span>Crescimento</span>
           </div>
           <div></div>
@@ -26,12 +26,12 @@ export function App() {
       </div>
       <div className="flex h-full max-w-[1150px] w-full px-10 items-center z-10">
 
-        <div className='text-5xl leading-[1.2em] font-bold'>
+        <div className='text-5xl leading-[1.2em] font-bold md:max-w-[400px]'>
           Em breve o novo portal <br />
           da <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#fffb9c] to-[#ffb69f]' /* style={{ background: `linear-gradient(90deg, #fffb9c, #ffb69f` }} */>IEC</span>  para sua <span className='bg-clip-text text-transparent bg-gradient-to-r from-[#c79cff] to-[#9fbfff]' >edificação</span>
         </div>
 
-        <div className='relative w-[40%] h-[50vh]'>
+        <div className='relative w-[40%] h-[50vh] max-md:hidden'>
 
 
           <img src={Img1} alt="" className='rounded-[10px] w-[65%] min-w-[250px] absolute left-auto top-0 right-[20%] bottom-0' />
